07bdcbb2b1963fbdacfb38822a42209589562662
William Baker <bakerw71@gmail.com>
2023-05-11 21:47:16 +0100

    prog gen

--- prog_gen.py
+++ prog_gen.py
@@ -33,9 +33,9 @@ lambda2 = TypeVar("lambda2")
 
 df = [
     # Class name,         input types,                 output type, weight
-    [ rasp.Map,          [SOp, lambda1],                  SOp     , 3],
-    [ rasp.Select,       [SOp, rasp.Predicate],           Selector, 3],
-    [ rasp.SequenceMap,  [SOp, SOpNumericValue, lambda2], SOp,      1],
+    [ rasp.Map,          [SOp, lambda1],                  SOp     , 2],
+    [ rasp.Select,       [SOp, SOp, rasp.Predicate],           Selector, 3],
+    [ rasp.SequenceMap,  [SOp, SOpNumericValue, lambda2], SOp,      4],
     [ rasp.Aggregate,    [Selector, SOp],                 SOp,      2],
     [ rasp.SelectorWidth,[Selector],                      SOp,      2],
     [ rasp.SelectorOr,   [Selector, Selector],            Selector, 1], 
@@ -44,7 +44,7 @@ df = [
 ]
 
 df = pd.DataFrame(df, columns = ['cls', 'inp', 'out', 'weight'])
-df_no_selector = pd.DataFrame(df[df.inp.apply(lambda x: Selector in x)])
+df_no_selector = pd.DataFrame(df[df.inp.apply(lambda x: Selector not in x)])
 
 
 # must ensure all basic comparison lambdas are present
@@ -61,7 +61,7 @@ df_no_selector = pd.DataFrame(df[df.inp.apply(lambda x: Selector in x)])
 #     [lambda x, y: x or y,  rasp.SOp.__or__,      [SOpNumericValue, SOpNumericValue], SOp,],
 # ]
 
-uni_lambdas = [
+UNI_LAMBDAS = [
     lambda x, y: x < y,  
     lambda x, y: x <= y,  
     lambda x, y: x > y,  
@@ -71,7 +71,7 @@ uni_lambdas = [
     lambda x, y: not x
 ]
 
-sequence_lambdas = [
+SEQUNCE_LAMBDAS = [
     lambda x, y: x + y,  
     lambda x, y: x * y,  
     lambda x, y: x - y,  
@@ -81,7 +81,7 @@ sequence_lambdas = [
 ]
 
 
-predicates = [
+PREDICATES = [
     rasp.Comparison.EQ,
     rasp.Comparison.FALSE,
     rasp.Comparison.TRUE,
@@ -95,8 +95,6 @@ predicates = [
 
 
 
-
-#%%
 
 # rasp.SOp.__lt__
 # rasp.SOp.__le__
@@ -112,86 +110,89 @@ class Cat(Enum):
     categoric = 2
     boolean = 3
 
-
+# TODO add a small chance of generating a const SOp / Selector
 class Scope:
-    def __init__(self, token_type: Cat) -> None:    
+    def __init__(self, vocabulary: Sequence[Union[str, int, bool]], max_seq_length) -> None:    
         self.scope = dict()
         self.names = set()
         self.types = set()
         self.counter = defaultdict(lambda: 0)
-        self.name_by_type = defaultdict(lambda: [])
+        self.names_by_type = defaultdict(lambda: [])
+        self.names_by_type_and_cat = defaultdict(lambda: defaultdict(lambda: []))
         self.type_cat = dict()
-        self.add(SOp, "tokens", token_type)
-        self.add(SOp, "indices", Cat.numeric)
+        if type(vocabulary[0]) == str:
+            token_type = Cat.categoric
+        else:
+            token_type = Cat.numeric
+        self.__add__(SOp, "tokens", token_type)
+        self.__add__(SOp, "indices", Cat.numeric)
+        self.max_seq_length = max_seq_length
+        self.vocabulary = vocabulary
 
     def add(self, t: type, cat: Cat) -> None:
         self.counter[t] += 1
-        return self.add(t, str(t) + ' ' + str(self.counter[t]), cat)
+        return self.__add__(t, str(t) + ' ' + str(self.counter[t]), cat)
     
-    def add(self, t: type, name: str, cat: Cat) -> None:
+    def __add__(self, t: type, name: str, cat: Cat) -> None:
         if name in self.names:
             raise ValueError(f'{name} is already present in the set')
         self.scope[name] = t
         self.type_cat[name] = cat
         self.names.add(name)
         self.types.add(t)
-        self.name_by_type[t].append(name)
+        self.names_by_type[t].append(name)
+        self.names_by_type_and_cat[t][cat].append(name)
         return name
     
     def get_cat(self, name: str):
         return self.type_cat[name]
     
-    def pick_var(self, y: type):
-        return choice(self.name_by_type[y])
+    def pick_var(self, y: type): # TODO add a small chance of generating a const SOp / Selector
+        return choice(self.names_by_type[y])
     
+    def pick_var_cat(self, y: type, cat: Cat): # TODO add a small chance of generating a const SOp / Selector
+        return choice(self.names_by_type_and_cat[y][cat])
+    
+    def var_exists(self, desired_type: type):
+        return len(self.names_by_type[desired_type]) > 0
+    
+    def var_exists_cat(self, desired_type: type, desired_cat: Cat):
+        return len(self.names_by_type_and_cat[desired_type][desired_cat]) > 0
+    
+    def matching_var_exists(self, src, desired_type: type):
+        """returns True if a variable with the desired type is in scope with category matching the source"""
+        return self.var_exists_cat(desired_type, self.get_cat(src))
+    
+    def gen_const(self, target_cat: Cat):
+        if target_cat == Cat.numeric:
+            return randint(0, self.max_seq_length)
+        elif target_cat == Cat.categoric:
+            return choice(self.vocabulary)
+        else:
+            raise NotImplementedError()
+
 @dataclass
 class Operation:
     operator: type
     inputs: Sequence[Union[str, Callable, rasp.Predicate ]]
     output: str
-    out_type: Cat
+
         
 
 
 
 
 
-scope = Scope({"tokens": SOp, "indices": SOp}, Cat.categoric)
+scope = Scope(list('abcd'), 6)
+ops = []
 
 
 
-
-def sample_function(scope: Scope, input_space_values, input_cat: Cat, max_seq_length: int, df=df):
-    sampled = df.sample(weights=df.weight)
-
-    return_type = None
-    for input_type in sampled.inp:
-        if input_type == SOp:
-            pass
-        if input_type == SOpNumericValue:
-            pass
-        if input_type == Selector:
-            pass
-        if input_type == lambda1:
-            chosen = choice(uni_lambdas)
-            return_type = Cat.boolean
-            obj_cat = scope.get_cat(inputs[0])
-            if inputs[0] == "tokens":
-                y = choice(input_space_values)
-            if inputs[0] == "indices":
-                y = randint(0, max_seq_length)
-            elif obj_cat == Cat.numeric:
-                y = randint(0, max_seq_length)
-            elif obj_cat == Cat.categoric:
-                y = choice(input_space_values)
-            func = partial(chosen, y)
-            inputs.append(func)
-            
-        if input_type == lambda2:
-            inputs.append(choice(sequence_lambdas))
-        if input_type == rasp.Predicate:
-            inputs.append(choice(predicates))
-            return_type = Cat.boolean
+def sample_function(scope: Scope, df=df):
+    if scope.var_exists(Selector):
+        sampled = df.sample(weights=df.weight).iloc[0]
+    else:
+        sampled = df_no_selector.sample(weights=df_no_selector.weight).iloc[0]
 
     
     if sampled.cls == rasp.Map:
@@ -200,35 +201,109 @@ def sample_function(scope: Scope, input_space_values, input_cat: Cat, max_seq_le
         return_type = SOp
 
         s1 = scope.pick_var(SOp)
-        f1 = choice(uni_lambdas)
+        f1 = choice(UNI_LAMBDAS)
         
         obj_cat = scope.get_cat(s1)
+        y = None
         if s1 == "tokens":
-            y = choice(input_space_values)
-        if s1 == "indices":
-            y = randint(0, max_seq_length)
+            y = scope.gen_const(Cat.categoric)
         elif obj_cat == Cat.numeric:
-            y = randint(0, max_seq_length)
+            y = scope.gen_const(Cat.numeric)
         elif obj_cat == Cat.categoric:
-            y = choice(input_space_values)
+            y = scope.gen_const(Cat.categoric)
+        elif obj_cat == Cat.boolean:
+            y = bool(randint(0,1))
+        else:
+            raise NotImplementedError()
         func = partial(f1, y)
 
         # Allocate a variable to hold the return value
         allocated_name = scope.add(return_type, return_cat)
         op = Operation(sampled.cls, [s1, func], allocated_name)
-              
+        ops.append(op)
+
+    elif sampled.cls == rasp.SequenceMap: # must have double the weight of Map
+        # TODO this might only accept numeric s1 inputs, but I guess we''ll find out
+        # [SOp, SOpNumericValue, lambda2],
+        # todo chance of const full selector/sop
+        s1 = scope.pick_var(SOp)
+        s1_cat = scope.get_cat(s1)
+        if scope.var_exists_cat(SOp, s1_cat) and randint(0,1) == 0: # s2 wil be an SOp
+            s2 = scope.pick_var_cat(SOp, s1_cat)
+        else: # s2 will be const
+            if  randint(0,2) <= 1: # 2/3 of the time generate an int
+                s2 = scope.gen_const(Cat.numeric)
+            else: # occasionally generate a float - may have a different impl
+                s2 = float(scope.gen_const(Cat.numeric)) + randint(0,100)/100 
+        f1 = choice(SEQUNCE_LAMBDAS)
+
+        return_type = SOp
+        return_cat = s1_cat
+
+        allocated_name = scope.add(return_type, return_cat)
+        op = Operation(sampled.cls, [s1, s2, f1], allocated_name)
+        ops.append(op)
 
     elif sampled.cls == rasp.Select:
-        # [SOp, rasp.Predicate],          
-    elif sampled.cls == rasp.SequenceMap:
-        # [SOp, SOpNumericValue, lambda2],
+        # [SOp, SOp, rasp.Predicate],    
+        # todo chance of const full selector/sop
+        s1 = scope.pick_var(SOp)
+        s2 = scope.pick_var_cat(SOp, scope.get_cat(s1))
+        pred = choice(PREDICATES)
+
+        return_type = Selector
+        return_cat = Cat.boolean
+
+        allocated_name = scope.add(return_type, return_cat)
+        op = Operation(sampled.cls, [s1, s2, pred], allocated_name)
+        ops.append(op)
+    
     elif sampled.cls == rasp.Aggregate:
-        # [Selector, SOp],                
+        # [Selector, SOp],
+        # todo chance of const full selector/sop
+        s1 = scope.pick_var(Selector)
+        s2 = scope.pick_var_cat(SOp, Cat.numeric)
+
+        return_type = SOp
+        return_cat = Cat.numeric
+
+        allocated_name = scope.add(return_type, return_cat)
+        op = Operation(sampled.cls, [s1, s2], allocated_name)
+        ops.append(op)
+
+
     elif sampled.cls == rasp.SelectorWidth:
         # [Selector],                     
-    elif sampled.cls == rasp.SelectorOr:
+        # todo chance of const full selector/sop
+        s1 = scope.pick_var(Selector)
+
+        return_type = SOp
+        return_cat = Cat.numeric
+
+        allocated_name = scope.add(return_type, return_cat)
+        op = Operation(sampled.cls, [s1], allocated_name)
+        ops.append(op)
+
+
+    elif (sampled.cls == rasp.SelectorOr) or (sampled.cls == rasp.SelectorAnd) or (sampled.cls == rasp.SelectorNot):
         # [Selector, Selector],           
-    elif sampled.cls == rasp.SelectorAnd:
-        # [Selector, Selector],           
-    elif sampled.cls == rasp.SelectorNot:
-        # [Selector, Selector],           
+        # todo chance of const full selector/sop
+        s1 = scope.pick_var(Selector)
+        s2 = scope.pick_var(Selector)
+
+        return_type = Selector
+        return_cat = Cat.boolean
+
+        allocated_name = scope.add(return_type, return_cat)
+        op = Operation(sampled.cls, [s1, s2], allocated_name)
+        ops.append(op)
+
+
+
+
+
+
+for i in range(0, 10):
+    sample_function(scope, df)
+
+#%%
