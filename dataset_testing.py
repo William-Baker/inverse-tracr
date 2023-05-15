7737ac17e1ac3e6bef29f7560052414fd34ba6e2
William Baker <bakerw71@gmail.com>
2023-05-15 17:41:11 +0100

    updated tests to v4 dataset

--- dataset_testing.py
+++ dataset_testing.py
@@ -3,21 +3,22 @@ from datasetv4 import craft_dataset
 gen, OP_VOCAB, VAR_VOCAB = craft_dataset()
 g = gen()
 
-while True:
-    weights, program = next(g)
+# while True:
+weights, program = next(g)
 
 #%%
 
 # test maximum time required to generate a program
-from dataset import craft_dataset
+from datasetv4 import craft_dataset
 import time
 import numpy as np
-gen, OP_VOCAB, VAR_VOCAB = craft_dataset()
+from tqdm import tqdm
+gen, OP_VOCAB, VAR_VOCAB = craft_dataset(ops_range=(20,20))
 g = gen()
 weights, program = next(g)
 
 times = []
-for i in range(10000):
+for i in tqdm(range(50)):
 	start = time.time()
 	weights, program = next(g)
 	end = time.time()
@@ -26,17 +27,5 @@ print(f"max time: {max(times)}")
 print(f"mean time: {np.mean(times)}")
 print(f"median time: {np.median(times)}")
 
-
-#%%
-import multiprocessing
-def a_function(ret_value):
-    ret_value.value = 3.145678
-
-ret_value = multiprocessing.Value("d", 0.0, lock=False)
-reader_process = multiprocessing.Process(target=a_function, args=[ret_value])
-reader_process.start()
-reader_process.join()
-
-print(ret_value.value)
 
 #%%
