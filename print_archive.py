4e9d4022df3c086938c1877dfcc6f6823cf2ea8c
William Baker <bakerw71@gmail.com>
2023-09-09 18:44:10 +0100

    parallel reading of dataset

--- print_archive.py
+++ print_archive.py
@@ -1,44 +1,4 @@
-# #%%
-# from data.parallelzipfile import ParallelZipFile as ZipFile
-# import cloudpickle
-
-# class ZipStreamReader:
-#     def __init__(self, dir:str) -> None:
-#         self.zip = ZipFile(file=dir, mode='r')
-#         self.files = sorted(self.zip.namelist())
-#     def __len__(self):
-#         return len(self.files)
-#     def __getitem__(self, idx):
-#         x = self.zip.read(self.files[idx])
-#         # loaded = np.load(BytesIO(x), allow_pickle=True)
-#         x,y = cloudpickle.loads(x)
-#         return x, y
-
-# #df = ZipStreamReader('cp_dataset_train_all.zip')
-
-# df = ZipStreamReader('cp_dataset_train_w.zip')
-# #df = ZipStreamReader('fixed.zip')
-# print(len(df))
-# it = iter(df)
-# for i in range(200):
-#     next(it)
-# for i in range(5):
-#     next(it)
-#     next(it)
-#     next(it)
-#     x,y = next(it)
-
-#     print(len(df))
-
-#     from data.dataloaders import ProgramEncoder
-#     print(x.keys())
-
-#     prog_enc = ProgramEncoder(15)
-#     print(prog_enc.decode_pred(y))
-# #df.zip.close()
-
 #%%
-
 from data.parallelzipfile import ParallelZipFile as ZipFile
 import cloudpickle
 
@@ -56,9 +16,49 @@ class ZipStreamReader:
 
 #df = ZipStreamReader('cp_dataset_train_all.zip')
 
-df = ZipStreamReader('.data/iTracr_dataset_v2_train.zip')
-#df = ZipStreamReader('.data/fixed.zip')
+df = ZipStreamReader('.data/dltest.zip')
+#df = ZipStreamReader('fixed.zip')
 print(len(df))
+it = iter(df)
+for i in range(200):
+    next(it)
+for i in range(5):
+    next(it)
+    next(it)
+    next(it)
+    x,y = next(it)
+
+    print(len(df))
+
+    from data.dataloaders import ProgramEncoder
+    print(x.keys())
+
+    prog_enc = ProgramEncoder(15)
+    print(prog_enc.decode_pred(y))
+#df.zip.close()
+
+#%%
+
+# from data.parallelzipfile import ParallelZipFile as ZipFile
+# import cloudpickle
+
+# class ZipStreamReader:
+#     def __init__(self, dir:str) -> None:
+#         self.zip = ZipFile(file=dir, mode='r')
+#         self.files = sorted(self.zip.namelist())
+#     def __len__(self):
+#         return len(self.files)
+#     def __getitem__(self, idx):
+#         x = self.zip.read(self.files[idx])
+#         # loaded = np.load(BytesIO(x), allow_pickle=True)
+#         x,y = cloudpickle.loads(x)
+#         return x, y
+
+# #df = ZipStreamReader('cp_dataset_train_all.zip')
+
+# df = ZipStreamReader('.data/dltest.zip')
+# #df = ZipStreamReader('.data/fixed.zip')
+# print(len(df))
 
 #%%
 
